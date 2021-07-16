5d4be07bfd05c95ff634a01a4434dbcd94f3fa71
Gerren <g.ballard23@outlook.com>
2021-07-16 14:19:32 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(stringName, eventName) {
+    let thankYouCard = [];
+    for(let i = 0; i < stringName.length; i++){
+        thankYouCard.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCard;
+}
+writeCards(["Dennis", "Jalon", "Gerren"], "birthday");
+
+function countDown() {
+    let i = 10
+    while (i>=0){
+        console.log(i);
+        i-=1;
+    }
+}
