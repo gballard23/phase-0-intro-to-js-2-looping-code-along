659703a9d85c20340d6988263cb0b37eb12ce6ca
Gerren <g.ballard23@outlook.com>
2021-08-21 11:05:55 -0500

    commit tree 9d4640b7f1de3f44bc42c1c9b6ad7d1569948326

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 // Code your solutions in this file
-function writeCards(stringName, eventName) {
-    let thankYouCard = [];
-    for(let i = 0; i < stringName.length; i++){
-        thankYouCard.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
-    }
-    return thankYouCard;
-}
-writeCards(["Dennis", "Jalon", "Gerren"], "birthday");
-
-function countDown() {
-    let i = 10
-    while (i>=0){
-        console.log(i);
-        i-=1;
-    }
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday me!`);
+    debugger;
 }
